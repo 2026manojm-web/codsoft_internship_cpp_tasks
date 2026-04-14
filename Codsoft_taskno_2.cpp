21c98e5d0364bfb0bb129d0879e6569224d4bae4
Manoj Kumar <2026manojm@gmail.com>
2026-04-14 22:14:22 +0530

    Normal Code

--- Codsoft_taskno_2.cpp
+++ Codsoft_taskno_2.cpp
@@ -1,1 +1,42 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1, num2;
+    char op;
+    cout << "***********************************  SIMPLE CALCULATOR  ***********************************\n\n";
+    cout << "Enter Number 1: ";
+    cin >> num1;
+    cout << "Enter Number 2: ";
+    cin >> num2;
+    cout << "Enter any Operation to Perform (+ - * /): ";
+    cin >> op;
+    if (op == '+')
+    {
+        cout << num1 + num2;
+    }
+    else if (op == '-')
+    {
+        cout << num1 - num2;
+    }
+    else if (op == '*')
+    {
+        cout << num1 * num2;
+    }
+    else if (op == '/')
+    {
+        if (num2 == 0)
+        {
+            cout << "Infinity";
+        }
+        else
+        {
+            cout << num1 / num2;
+        }
+    }
+    else
+    {
+        cout << "Try a Different operator only (+ - * /) are allowed";
+    }
+    cout << "\n\n***************************************  THANK YOU  ***************************************";
+}
